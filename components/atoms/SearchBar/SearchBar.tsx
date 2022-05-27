9d4dfd438dd0629a0766a099e03d012a3ca512fe
DESKTOP-VI6LSH9\NoraHajdu <hajdu.nora115@gmail.com>
2022-05-27 19:09:24 +0200

    Hardcode Titanic search

--- components/atoms/SearchBar/SearchBar.tsx
+++ components/atoms/SearchBar/SearchBar.tsx
@@ -1,12 +1,49 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
 
 interface ISearchBar {}
 
+const createQuery = (term:string) => gql`
+query SearchMovies {
+  searchMovies(query: "${term}") {
+    id
+    name
+    overview
+    releaseDate
+    cast {
+      id
+      person {
+        name
+      }
+      role {
+        ... on Cast {
+          character
+        }
+      }
+    }
+  }
+}
+`
+
 const SearchBar: React.FC<ISearchBar> = () => {
+  const { data, loading, error } = useQuery(createQuery('Titanic'))
+
+  if (loading) return 'Loading...'
+  if (error) return <pre>{error.message}</pre>
+
   return (
     <div>
       SEARCH
       <input type="text" />
+      <ul>
+        {data.searchMovies.map(({ name, category, score }) => {
+          return <>
+          <div>{name}</div>
+          <div>{category}</div>
+          <div>{score}</div>
+          </>
+        })}
+      </ul>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
-function MyApp ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const client = new ApolloClient({
+  uri: 'https://tmdb.sandbox.zoosh.ie/dev/grphql',
+  cache: new InMemoryCache()
+})
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
